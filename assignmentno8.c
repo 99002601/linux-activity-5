ed8c3ac457e9155c9caedb56c8ff2ab6d7729d7a
99002601 <71483075+99002601@users.noreply.github.com>
2020-10-19 09:18:58 +0530

    Create assignmentno8.c

---
+++ assignmentno8.c
@@ -0,0 +1,54 @@
+#include <pthread.h> 
+#include <stdio.h> 
+  
+#define max 16 
+  
+#define Th_max 4 
+  
+
+int a[max] = { 1, 5, 7, 10, 12, 14, 15, 18, 20, 
+               22, 25, 27, 300, 64, 110, 220 }; 
+  
+
+int max_num[Th_max] = { 0 }; 
+int thread_no = 0; 
+  
+void maximum(void* arg) 
+{ 
+    int i, number = thread_no++; 
+    int maxs = 0; 
+  
+    for (i = number * (max / 4); i < (number + 1) * (max / 4); i++) { 
+        if (a[i] > maxs) 
+            maxs = a[i]; 
+    } 
+  
+    max_num[number] = maxs; 
+} 
+  
+
+int main() 
+{ 
+    int maxs = 0; 
+    int i; 
+    pthread_t threads[Th_max]; 
+  
+   
+    for (i = 0; i < Th_max; i++) 
+        pthread_create(&threads[i], NULL, 
+                       maximum, (void*)NULL); 
+  
+   
+    for (i = 0; i < Th_max; i++) 
+        pthread_join(threads[i], NULL); 
+  
+  
+    for (i = 0; i < Th_max; i++) { 
+        if (max_num[i] > maxs) 
+            maxs = max_num[i]; 
+    } 
+  
+    printf("Maximum Element is : %d", maxs); 
+  
+    return 0; 
+} 
