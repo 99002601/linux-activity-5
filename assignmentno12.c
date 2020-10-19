a489e5099fe0c2c4548f527831d2745c8802ef29
99002601 <71483075+99002601@users.noreply.github.com>
2020-10-19 09:13:47 +0530

    Create assignmentno12.c

---
+++ assignmentno12.c
@@ -0,0 +1,24 @@
+#include <stdio.h> 
+#include <sys/resource.h> 
+#include <string.h> 
+#include <errno.h> 
+#include <unistd.h> 
+#include <sys/types.h> 
+#include <sys/stat.h> 
+#include <fcntl.h> 
+  
+int main() 
+{ 
+  
+    struct rlimit lim; 
+    //Get limits andd print
+    if( getrlimit(RLIMIT_NOFILE, &lim) == 0) 
+        printf("SL= %ld \t"
+           " HL= %ld \n", lim.rlim_cur,  
+                                 lim.rlim_max); 
+    else
+        fprintf(stderr, "%s\n", strerror(errno)); 
+      
+
+    return 0; 
+} 
